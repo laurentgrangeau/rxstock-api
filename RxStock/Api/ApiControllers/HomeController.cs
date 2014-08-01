b804261ec9df0bb7fb2799d15ad7017bac8a4a70
laurentgrangeau <laurent.grangeau@gmail.com>
2014-08-01 16:01:32 +0200

    Add api controller

---
+++ RxStock/Api/ApiControllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace Api.ApiControllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    public class HomeController : ApiController
+    {
+        public IList<string> Get()
+        {
+            return new List<string>
+            {
+                "1",
+                "2"
+            };
+        }
+    }
+}
