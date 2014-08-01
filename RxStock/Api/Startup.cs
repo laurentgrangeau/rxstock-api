35ab7df124c1bbfe72bcca01f252e91cff898b0c
laurentgrangeau <laurent.grangeau@gmail.com>
2014-08-01 14:47:32 +0200

    Meet Helios ! The new OWIN implementation of IIS

---
+++ RxStock/Api/Startup.cs
@@ -0,0 +1,26 @@
+﻿namespace Api
+{
+    using Owin;
+    using System;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.Run(async context =>
+            {
+                context.Response.ContentType = "text/plain";
+
+                await context.Response.WriteAsync("Assemblies in AppDomain:" + Environment.NewLine);
+                foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    await context.Response.WriteAsync(asm.FullName + Environment.NewLine);
+                }
+                await context.Response.WriteAsync(Environment.NewLine);
+
+                await context.Response.WriteAsync("Stack trace:" + Environment.NewLine);
+                await context.Response.WriteAsync(Environment.StackTrace);
+            });
+        }
+    }
+}
