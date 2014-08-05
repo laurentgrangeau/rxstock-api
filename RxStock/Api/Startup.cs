f4c923dc09e4ef2e24cd14c2834494ea19bde717
laurentgrangeau <laurent.grangeau@gmail.com>
2014-08-05 14:35:52 +0200

    Update NuGet packages and remove XmlFormatter

--- RxStock/Api/Startup.cs
+++ RxStock/Api/Startup.cs
@@ -10,6 +10,7 @@
             var httpConfiguration = new HttpConfiguration();
 
             httpConfiguration.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+            httpConfiguration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
 
             appBuilder.UseWebApi(httpConfiguration);
         }
